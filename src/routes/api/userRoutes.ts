6c84a998ded971fddb7c93a0a941224fb14122e0
achensen <Erinrjacobsen@gmail.com>
2025-02-19 13:41:35 -0500

    delete friend routing

--- src/routes/api/userRoutes.ts
+++ src/routes/api/userRoutes.ts
@@ -7,6 +7,7 @@ import {
   deleteUser,
   updateUser,
   addFriend,
+  deleteFriend,
 
 } from '../../controllers/userController.js';
 
@@ -18,6 +19,9 @@ router.route('/:userId').get(getUserById).delete(deleteUser).put(updateUser);
 
 // /api/users/:userId/friends/:friendId
 router.route('/:userId/friends/:friendId').post(addFriend);
+//api 
+router.route('/:userId/friends/:friendId').delete(deleteFriend);
+
 
 
 export { router as userRouter} ;
